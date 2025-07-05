5c56b3b5b1df15f6e733b983eea95e919ed7d0d6
HDUC122002 <huynhhducc2002@gmail.com>
2025-07-05 21:41:48 +0700

    modify domains

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiBaseUrl: 'http://localhost:8080/api/v1',
-    apiUrl: 'http://localhost:8080/api/v1',
+    apiBaseUrl: 'https://ai6.vn/api/v1',
+    apiUrl: 'https://ai6.vn/api/v1',
     activityFeedUrl: 'http://localhost:3001',
     // Chọn backend cho Activity Feed
     useMainBackendForActivity: true, // true = Java backend, false = Node.js backend

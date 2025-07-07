c382f1ae2d43dd58789e0a06d687075042ad27ad
An Pham <118990514+Games-on@users.noreply.github.com>
2025-07-07 14:38:07 +0700

    fix cors

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
     apiBaseUrl: 'https://api-v1.ai6.vn/api/v1',
+    // apiBaseUrl: 'http://localhost:8080/api/v1',
+    // apiUrl: 'http://localhost:8080',
     apiUrl: 'https://api-v1.ai6.vn',
     activityFeedUrl: 'http://localhost:3001',
-    useMainBackendForActivity: true, // true = Java backend, false = Node.js backend
+    useMainBackendForActivity: true, 
     production: false
 }

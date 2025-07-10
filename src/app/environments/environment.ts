2abb3908c96ea238fee19761c1d7302ca1ad1a12
An Pham <118990514+Games-on@users.noreply.github.com>
2025-07-10 15:08:39 +0700

    fix env

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-
     // apiBaseUrl: 'http://localhost:8080/api/v1', // test local thì bật cái này
     // apiUrl: 'http://localhost:8080', // test local thì bật cái này
     apiBaseUrl: 'https://api-v1.ai6.vn/api/v1', // production thì bật cái này

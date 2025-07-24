c746c0e5b7d02b78a9fa82c50fbafff2c272d0ce
An Pham <118990514+Games-on@users.noreply.github.com>
2025-07-24 09:08:13 +0700

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,9 +8,9 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component'; 
 import { ChatBoxComponent } from '../../components/chat-box/chat-box.component'; 
 import { TopScamService, TopScamItem } from '../../services/top-scam.service';
-// import { VerticalBannerComponent } from './vertical-banner/vertical-banner.component';
+import { VerticalBannerComponent } from './vertical-banner/vertical-banner.component';
 import { ActivityWidgetComponent } from '../activity-widget/activity-widget.component';
-import { Title, Meta } from '@angular/platform-browser'; // Import Title và Meta service
+import { Title, Meta } from '@angular/platform-browser'; 
 
 interface ScamDetail {
   id: number;
@@ -55,7 +55,7 @@ interface Message {
     HeaderComponent,
     FooterComponent,
     ChatBoxComponent,
-    // VerticalBannerComponent,
+    VerticalBannerComponent,
     ActivityWidgetComponent
 
   ],
